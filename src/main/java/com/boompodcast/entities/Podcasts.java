ad088d62960fa6d5f70081446abbfdedd86d3c4d
higor021 <79275174+higor021@users.noreply.github.com>
2021-06-08 18:45:26 -0300

    Entidade Categories

--- src/main/java/com/boompodcast/entities/Podcasts.java
+++ src/main/java/com/boompodcast/entities/Podcasts.java
@@ -1,9 +1,11 @@
 package com.boompodcast.entities;
 
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 public class Podcasts {
 
 	@ManyToOne
+	@JoinColumn(name = "categories_id")
 	private Categories categories;
 }
