35b8d68070c27e977ca1f068f5371381e15695bc
higor021 <79275174+higor021@users.noreply.github.com>
2021-06-08 17:06:03 -0300

    Projeto criado

---
+++ src/main/java/com/boompodcast/BoompodcastApplication.java
@@ -0,0 +1,13 @@
+package com.boompodcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoompodcastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoompodcastApplication.class, args);
+	}
+
+}
