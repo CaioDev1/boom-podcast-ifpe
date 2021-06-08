007f2e41a502ac054a42ab58ba7c6472a5ea00ac
pedrolucas-sa <47487770+pedrolucas-sa@users.noreply.github.com>
2021-06-08 19:12:30 -0300

    Add files via upload

---
+++ User.java
@@ -0,0 +1,41 @@
+package com.boompodcast.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import boomPodcast.ifpe.pp2.user.Level;
+@Entity
+public class User {
+	@Id
+	private Integer id;
+	private	String username;
+	private	String email;
+	private	String password;
+	@ManyToOne
+	private Level level;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+}
