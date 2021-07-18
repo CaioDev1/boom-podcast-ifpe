bac0e4a3808c5cec636deb67f11c87db6f6bba22
Caio <imcaiofelipe@gmail.com>
2021-07-18 18:39:35 -0300

    correções adminkey

--- src/main/java/app_paths/AppPaths.java
+++ src/main/java/app_paths/AppPaths.java
@@ -1,5 +1,5 @@
 package app_paths;
 
 public class AppPaths {
-	public static final String firebaseConfigPath = System.getProperty("user.dir").concat("/src/main/java/ifpe-boompodcast-firebase-adminsdk.json");
+	public static final String firebaseConfigPath = System.getProperty("user.dir").concat("/ifpe-boompodcast-firebase-adminsdk.json");
 }
